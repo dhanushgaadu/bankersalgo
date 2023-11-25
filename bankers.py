0e25fcf5c4302d3a24925d1048dd3168f2d8847d
dhanush natra <146715861+dhanushgaadu@users.noreply.github.com>
2023-11-25 11:56:59 +0530

    Update bankers.py

--- bankers.py
+++ bankers.py
@@ -26,26 +26,6 @@ def calculate_safe_sequence(available, alloc, max_req, n_processes, n_resources)
                 break
     return safe_sequence
 
-# example usage
-alloc = [[0, 1, 0],
-         [2, 0, 0],
-         [3, 0, 2],
-         [2, 1, 1],
-         [0, 0, 2]]
-
-max_req = [[7, 5, 3],
-           [3, 2, 2],
-           [9, 0, 2],
-           [2, 2, 2],
-           [4, 3, 3]]
-
-available = [10, 5, 7]
-
-n_processes = 5
-n_resources = 3
-
-safe_sequence = calculate_safe_sequence(available, alloc, max_req, n_processes, n_resources)
-
 if type(safe_sequence) == str:
     print(safe_sequence)
 else:
